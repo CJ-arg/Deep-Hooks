c1dd626a78e7b4ef88dc375a60358294a3948070
unknown <janoncarlos@gmail.com>
2023-07-08 19:49:35 -0300

    todo primera parte

--- src/07-tarea-memo/Padre.jsx
+++ src/07-tarea-memo/Padre.jsx
@@ -3,26 +3,19 @@ import { Hijo } from './Hijo'
 import { useState } from 'react';
 
 export const Padre = () => {
-
-    const numeros = [2, 4, 6, 8, 100];
+    const numeros = [2, 40, 600, 8000, 10000];
     const [valor, setValor] = useState(0);
-
-
     const padreIncrementar = useCallback(
         (numero) => {
 
             setValor((valor) => valor + numero)
         },
-        [],
-    )
-
+        [],)
     return (
         <div>
             <h1>Padre</h1>
             <p> Total: {valor} </p>
-
             <hr />
-
             {
                 numeros.map(n => (
                     <Hijo
---
+++ src/08-useReducer/TodoApp.jsx
@@ -0,0 +1,61 @@
+import React, { useReducer } from 'react'
+import { todoReducer } from './ToDoReducer'
+
+const initialState = [{
+  id: new Date().getTime(),
+  description: "Recolectar la piedra del Tiempo",
+  done: false,
+},
+{
+  id: new Date().getTime() * 3,
+  description: "Recolectar la piedra del Alma",
+  done: false,
+}
+]
+
+const TodoApp = () => {
+  const [state, dispatch] = useReducer(todoReducer, initialState)
+
+  return (
+    <>
+      <h1>TodoApp: 10 --<small> Pendientes: 2</small></h1>
+      <hr />
+      <div className="row">
+        <div className="col-7">
+
+          <ul className='list-group'>
+
+            {todos.map(todo => {
+
+
+              <li key={todo.id} className='list-group-item d-flex justify-content-between'> <span className='align-self-center'> Item 1</span> <button className='btn btn-danger btn-sm'>Borrar</button></li>
+
+
+            })}
+
+
+            <li key={todo.id} className='list-group-item d-flex justify-content-between'><span className='align-self-center'> Item 2</span> <button className='btn btn-danger btn-sm'>Borrar</button> </li>
+            <li className='list-group-item d-flex justify-content-between'><span className='align-self-center'> Item 3</span><button className='btn btn-danger btn-sm'>Borrar</button> </li>
+          </ul>
+
+        </div>
+
+
+        <div className="col-5">
+          <h4>Agregar TODO</h4>
+          <hr />
+          <form >
+            <input type="text"
+              placeholder='Ingresa una tarea para hacer?'
+              className='form-control'
+            />
+            <button type='submit' className='btn btn-outline-primary mt-3'> AGREGAR</button>
+          </form>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default TodoApp
