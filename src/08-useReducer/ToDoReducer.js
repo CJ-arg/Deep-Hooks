2887b15a6bf415818b92d3b250e46eee3f8067a6
unknown <janoncarlos@gmail.com>
2023-07-10 15:37:25 -0300

    add local storage

--- src/08-useReducer/ToDoReducer.js
+++ src/08-useReducer/ToDoReducer.js
@@ -2,7 +2,8 @@ export const todoReducer = (initialState = [], action) => {
   switch (action.type) {
     case "[TODO] Add Todo":
       return [...initialState, action.payload];
-
+    case "[TODO] Remove Todo":
+      return [...initialState, action.payload];
     default:
       return initialState;
   }
