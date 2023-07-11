25d0a6f403c430f7cd6c960d2f68316a4a78bd7a
unknown <janoncarlos@gmail.com>
2023-07-11 16:36:58 -0300

    setUser

--- src/09-useContext/AboutPage.jsx
+++ src/09-useContext/AboutPage.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from './context/UserContext'
 
 const AboutPage = () => {
+  const { user, setUser } = useContext(UserContext)
+
   return (
     <>
-      <h1>AboutPage</h1>
+      <h1>AboutPage </h1>
       <hr />
+      <pre>
+        {JSON.stringify(user, null, 3)}
+      </pre>
+      <button className='btn btn-primary'
+        onClick={() => setUser({
+          id: 123,
+          name: 'Pedro Picapiedra',
+          mail: 'cjmail@gmail.com'
+        })}> Set User</button>
     </>
-
   )
 }
 
