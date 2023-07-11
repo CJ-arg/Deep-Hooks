50b1579922cc34c2bc2cf5500243bc5f00f74eea
unknown <janoncarlos@gmail.com>
2023-07-11 16:33:15 -0300

    setUser

--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import { UserContext } from './context/UserContext';
 
 const LoginPage = () => {
-  const { user } = useContext(UserContext)
+  const { user, setUser } = useContext(UserContext)
 
   return (
     <>
@@ -12,7 +12,12 @@ const LoginPage = () => {
       <pre>
         {JSON.stringify(user, null, 3)}
       </pre>
-      <button className='btn btn-primary '> Set User</button>
+      <button className='btn btn-primary'
+        onClick={() => setUser({
+          id: 123,
+          name: 'carlos Janon',
+          mail: 'cjmail@gmail.com'
+        })}> Set User</button>
     </>
   )
 }
